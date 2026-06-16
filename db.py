d6aeb7bd851c012f89d39b6d1c03eb7e6d7ede40
morycal <k95230387@gmail.com>
2026-06-16 13:52:47 +0330

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,32 @@
-import os
-import psycopg2
+import sqlite3
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+conn = sqlite3.connect("music.db", check_same_thread=False)
+c = conn.cursor()
 
-def conn():
-    return psycopg2.connect(DATABASE_URL)
+c.execute("""
+CREATE TABLE IF NOT EXISTS likes (
+    user_id TEXT,
+    title TEXT,
+    url TEXT
+)
+""")
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS history (
+    user_id TEXT,
+    title TEXT,
+    url TEXT
+)
+""")
+
+conn.commit()
+
+
+def add_like(user_id, title, url):
+    c.execute("INSERT INTO likes VALUES (?, ?, ?)", (user_id, title, url))
+    conn.commit()
+
+
+def get_likes(user_id):
+    c.execute("SELECT title, url FROM likes WHERE user_id=?", (user_id,))
+    return c.fetchall()
