fab57324ab37566a7b27ecc7ab2292918914d62f
morycal <k95230387@gmail.com>
2026-06-16 15:09:43 +0330

    Update music.py

--- music.py
+++ music.py
@@ -2,17 +2,30 @@ import subprocess
 
 def search_music(query):
     result = subprocess.getoutput(
-        f'yt-dlp "ytsearch3:{query}" --get-title --get-url'
+        f'yt-dlp "ytsearch1:{query}" --get-title --get-url'
     ).split("\n")
 
-    songs = []
-    for i in range(0, len(result)-1, 2):
-        songs.append({
-            "title": result[i],
-            "url": result[i+1]
-        })
+    if len(result) >= 2:
+        return {
+            "type": "audio",
+            "title": result[0],
+            "url": result[1]
+        }
 
-    return songs
+    return None
+
+
+def download_video(query):
+    filename = "video.mp4"
+
+    subprocess.call([
+        "yt-dlp",
+        "-f", "mp4",
+        "-o", filename,
+        f"ytsearch1:{query}"
+    ])
+
+    return filename
 
 
 def get_stream(url):
