9b5d15d2014be0db48d427f1d537e1efe7bbe4f1
morycal <k95230387@gmail.com>
2026-06-20 17:58:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ while True:
                 elif text == "خوبی":
                     reply = "ممنون، خوبم 😊"
                 else:
-                    reply = \n{text}"
+                    reply = f"\n{text}"
 
             send_message(
                 chat_id,
